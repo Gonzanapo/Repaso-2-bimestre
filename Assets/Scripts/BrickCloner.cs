5e4f5f78ee1339003f71ebef52e6aa1e1af824f1
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2022-05-17 15:54:00 -0300

    cominezo del respaso

---
+++ Assets/Scripts/BrickCloner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickCloner : MonoBehaviour {
+    public GameObject brick;
+    public Text ingresoCant;
+    public float xPosicion = 0;
+
+
+    private void Start()
+    {
+     
+    }
+
+
+    public void brickCloner()
+    {
+        
+        GameObject clone;
+       
+        
+        for (int i = 0; i > float.Parse(ingresoCant.text); i++)
+        {
+            clone = Instantiate(brick);
+            clone.transform.position = new Vector3(xPosicion, 0, 0);
+            xPosicion += xPosicion + 1.1f;
+            Destroy(clone,2);
+        }
+
+        
+    }
+	
+      
+
+	
+	
+}
+
+
+/*
+ * 
+*/
